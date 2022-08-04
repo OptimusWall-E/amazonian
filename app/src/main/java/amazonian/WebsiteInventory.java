21e31cd3074967fcc34dad32c82b4f695723723e
charlotte buchan <cebuc@talktalk.net>
2022-08-04 14:45:05 +0100

    add product test passed

--- app/src/main/java/amazonian/WebsiteInventory.java
+++ app/src/main/java/amazonian/WebsiteInventory.java
@@ -6,6 +6,11 @@ public class WebsiteInventory {
 
   private HashMap<Product, Integer> items = new HashMap<Product, Integer>();
 
+
+  public void addProduct(Product item, int quantity){
+    this.items.put(item, quantity);
+  }
+
   public HashMap<Product, Integer> viewProducts(){
     return this.items;
   }
